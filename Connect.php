215b49096182d6546b30f364d84aece8c094138c
silbae <silbae@schuldorf.de>
2025-06-03 07:25:29 +0000

    Changed mysqli to pdo

--- Connect.php
+++ Connect.php
@@ -10,15 +10,7 @@ class Connect {
 
     public function connect() { 
         // Create connection 
-        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname); 
-
-        // Check connection 
-
-        if ($this->conn->connect_error) { 
-
-            die("Connection failed: " . $this->conn->connect_error); 
-
-        } 
+        $this->conn = new PDO('mysql:host='.$this->servername.';dbname='.$this->dbname, $this->username, $this->password); 
     }
 
     public function query($stm)
