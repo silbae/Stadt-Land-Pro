4453de9f0493e17fe2042aa33c553a97bc9a888e
silbae <silbae@schuldorf.de>
2025-06-06 08:15:17 +0000

    bug fix login

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if(isset($_GET['login'])) {
     $email = $_POST['email'];
     $passwort = $_POST['passwort'];
     
-    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $statement = $pdo->prepare("SELECT * FROM Benutzer WHERE Email = :email");
     $result = $statement->execute(array('email' => $email));
     $user = $statement->fetch();
         
