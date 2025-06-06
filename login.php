2a3eeb8b95280db3ba118d47b19eb738446a9221
silbae <silbae@schuldorf.de>
2025-06-06 10:23:10 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,23 +3,23 @@ require_once("Connect.php");
 session_start();
 $conn = new Connect(); 
 $conn->connect();
- 
+
 if(isset($_GET['login'])) {
     $email = $_POST['email'];
     $passwort = $_POST['passwort'];
-    
-    $statement = $user = $conn->queryPrep("SELECT * FROM Benutzer WHERE Email = :email");
+
+    // SQL-Statement mit korrekten Spaltennamen
+    $statement = $conn->queryPrep("SELECT * FROM Benutzer WHERE Email = :email");
     $result = $statement->execute(array('email' => $email));
     $user = $statement->fetch();
-        
-    //Überprüfung des Passworts
-    if ($user !== false && password_verify($passwort, $user['passwort'])) {
-        $_SESSION['userid'] = $user['id'];
+
+    // Überprüfung des Passworts – Spaltenname "Passwort"!
+    if ($user !== false && password_verify($passwort, $user['Passwort'])) {
+        $_SESSION['benutzer'] = $user['Benutzer'];
         die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
     } else {
         $errorMessage = "E-Mail oder Passwort war ungültig<br>";
     }
-    
 }
 ?>
 <!DOCTYPE html> 
@@ -28,20 +28,20 @@ if(isset($_GET['login'])) {
   <title>Login</title>    
 </head> 
 <body>
- 
+
 <?php 
 if(isset($errorMessage)) {
     echo $errorMessage;
 }
 ?>
- 
+
 <form action="?login=1" method="post">
 E-Mail:<br>
 <input type="email" size="40" maxlength="250" name="email"><br><br>
- 
+
 Dein Passwort:<br>
 <input type="password" size="40"  maxlength="250" name="passwort"><br>
- 
+
 <input type="submit" value="Abschicken">
 </form> 
 </body>
