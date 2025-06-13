0ff6b0847aad560b3f44210b59b81bb3e039e8cc
KilSti <kilsti@schuldorf.de>
2025-06-13 10:12:32 +0200

    Update eingabefeld.php

--- eingabefeld.php
+++ eingabefeld.php
@@ -13,6 +13,7 @@ while ($kategorie = $stm->fetch_assoc()){
 
 print_r($kategorien);
 
+/**
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['word']) && isset($_POST['Kategorie'])) {
     $word = $_POST['word'];
     $kategorie = $_POST['Kategorie'];
@@ -23,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['word']) && isset($_POS
 
     echo "Wort erfolgreich gespeichert!";
 }
+*/
 ?>
 
 <!DOCTYPE html>
