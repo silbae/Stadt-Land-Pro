850df0229b82d0b7eb106ce08519132e5e2d3318
silbae <silbae@schuldorf.de>
2025-06-15 14:31:06 +0200

    comment geheim.php

--- geheim.php
+++ geheim.php
@@ -1,4 +1,4 @@
-<?php //Silas
+<?php //Silas (ungenutz, da nach dem Login sofort eine Weiterleitung zu suchleiste.php erfolgt)
 session_start();
 if(!isset($_SESSION['userid'])) {
     die('Bitte zuerst <a href="login.php">einloggen</a>');
