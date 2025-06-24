5ba3245fe7172e329294bb9464c9383940442aa6
silbae <silbae@schuldorf.de>
2025-06-24 09:08:07 +0200

    Update eingabefeld.php

--- eingabefeld.php
+++ eingabefeld.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['word']) && isset($_PO
     // XP vergeben, wenn Benutzer angemeldet ist
 if (isset($_SESSION['email'])) {
     $user_email = $_SESSION['email'];
-    $db->insert("UPDATE Benutzer SET Xp = Xp + 12 WHERE Email = :email", [':email' => $user_email]);
+    $db->insert(UPDATE Benutzer SET Xp = Xp + 12 WHERE Email = '$user_email'", [':email' => $user_email]);
 }
 
     echo '<div class="success-message">Wort erfolgreich gespeichert!</div>';
